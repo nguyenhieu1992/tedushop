2efff6d08edf6cd40edcad96a9d03a9937b8aadb
hieupro_bs <hieungn84@gmail.com>
2021-10-18 09:06:07 +0700

    add more repository for other tables

---
+++ TeduShop.Data/Repositories/ApplicationGroupRepository.cs
@@ -0,0 +1,16 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IApplicationGroupRepository : IRepository<ApplicationGroup>
+    {
+    }
+
+    public class ApplicationGroupRepository : RepositoryBase<ApplicationGroup>, IApplicationGroupRepository
+    {
+        public ApplicationGroupRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ TeduShop.Data/Repositories/ApplicationRoleRepository.cs
@@ -0,0 +1,16 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IApplicationRoleRepository : IRepository<ApplicationRole>
+    {
+    }
+
+    public class ApplicationRoleRepository : RepositoryBase<ApplicationRole>, IApplicationRoleRepository
+    {
+        public ApplicationRoleRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
